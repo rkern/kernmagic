61e2ef348425d81c53754b8f66b58bb9db361203
Robert Kern <rkern@enthought.com>
2013-03-19 09:59:10 +0000

    ENH: Add version number.

--- setup.py
+++ setup.py
@@ -5,5 +5,6 @@ setup(
     description="Robert Kern's IPython magics.",
     author="Robert Kern",
     author_email="robert.kern@enthought.com",
+    version="0.2.0",
     packages=find_packages(),
 )
